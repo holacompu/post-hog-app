a10659814194751f0a9cf636533b29e8c55366a8
Dani V. <205372581+holacompu@users.noreply.github.com>
2025-04-01 06:36:52 -0500

    Add hogname validation form

--- vite-hog/src/App.tsx
+++ vite-hog/src/App.tsx
@@ -4,8 +4,23 @@ import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 
+import { Button, Checkbox, Group, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
 export default function App() {
-  return <MantineProvider>{
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/hog/i.test(value) ? null : 'Invalid hogname'),
+    },
+  });
+
+  return <MantineProvider> {
     
       (
         <>
@@ -15,36 +30,29 @@ export default function App() {
           <p>
             No more button to hog++. Sad.
           </p>
+
+    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <TextInput
+        withAsterisk
+        label="Hogname (must contain 'hog')"
+        placeholder="TheSmilingHog"
+        key={form.key('email')}
+        {...form.getInputProps('email')}
+      />
+
+      <Checkbox
+        mt="md"
+        label="I agree to have fun"
+        key={form.key('termsOfService')}
+        {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+      />
+
+      <Group justify="flex-end" mt="md">
+        <Button type="submit">Sign up</Button>
+      </Group>
+    </form>
         </>
       )
   
   }</MantineProvider>;
 }
-
-
-
-
-
-{/*
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Hello Hog!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Hogcount is {count} 🦔
-        </button>
-      </div>
-      <p>
-        Click on the button to hog++
-      </p>
-    </>
-  )
-}
-
-export default App
-
-*/}
