f2dc15bbb4922bf0377370ff226f2ba20a44605b
Dani V. <205372581+holacompu@users.noreply.github.com>
2025-04-06 14:10:41 -0500

    Implement AppShell + MobileNavbar

--- vite-hog/src/App.tsx
+++ vite-hog/src/App.tsx
@@ -5,10 +5,15 @@ import { useState } from 'react'
 
 import { MantineProvider } from '@mantine/core';
 
-import { Button, Checkbox, Group, TextInput } from '@mantine/core';
+import { AppShell, Burger, Button, Checkbox, Group, TextInput, UnstyledButton } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+import ReactLogo from './assets/react.svg';
+import classes from './MobileNavbar.module.css';
 
 export default function App() {
+
+  const [opened, { toggle }] = useDisclosure();
   
   //Signup form with Hogname + ToS validation
   let chosenHogName: string[] = [];
@@ -30,43 +35,76 @@ export default function App() {
 
   return <MantineProvider> {
     
-      (
-        <>
+    (
+      <>
+
+<AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group justify="space-between" style={{ flex: 1 }}>
+          
+            <Group ml="xl" gap={0} visibleFrom="sm">
+              <UnstyledButton className={classes.control}>Home</UnstyledButton>
+              <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+              <UnstyledButton className={classes.control}>Contacts</UnstyledButton>
+              <UnstyledButton className={classes.control}>Support</UnstyledButton>
+            </Group>
+          </Group>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar py="md" px={4}>
+        <UnstyledButton className={classes.control}>Home</UnstyledButton>
+        <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+        <UnstyledButton className={classes.control}>Contacts</UnstyledButton>
+        <UnstyledButton className={classes.control}>Support</UnstyledButton>
+      </AppShell.Navbar>
+
+        <AppShell.Main>
           <h1>Post hogs 🦔</h1>
-
-          <p>The <u>only</u> social media where you're <u>only</u> allowed to post hedgehogs.</p>
-
+          <p>
+            The <u>only</u> social media where you're <u>only</u> allowed to post hedgehogs.
+          </p>
           <Button onClick={() => setCount((count) => count + 1)}>
-          <code>hog++</code>
+            <code>hog++</code>
           </Button>
-          <p><code>hog++</code> value = <b>{count}</b> 🦔s
+          <p>
+            <code>hog++</code> value = <b>{count}</b> 🦔s
           </p>
 
 
-    <form onSubmit={form.onSubmit((values) => (console.log(values)))}>
-      <TextInput
-        withAsterisk
-        label="Sign up by typing your Hogname below 👇"
-        placeholder="Hogname (must contain 'hog')"
-        key={form.key('hogName')}
-        {...form.getInputProps('hogName')}
-      />
-          
-      <Checkbox
-        mt="md"
-        label="I agree to have fun"
-        key={form.key('termsOfService')}
-        {...form.getInputProps('termsOfService', { type: 'checkbox' })}
-      />
+            <form onSubmit={form.onSubmit((values) => (console.log(values)))}>
+              <TextInput
+                withAsterisk
+                label="Sign up by typing your Hogname below 👇"
+                placeholder="Hogname (must contain 'hog')"
+                key={form.key('hogName')}
+                {...form.getInputProps('hogName')}
+              />
+                  
+              <Checkbox
+                mt="md"
+                label="I agree to have fun"
+                key={form.key('termsOfService')}
+                {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+              />
 
-      <Group justify="flex-end" mt="md">
-        <Button type="submit">Sign up</Button>
-      </Group>
-    </form>
+              <Group justify="flex-end" mt="md">
+                <Button type="submit">Sign up</Button>
+              </Group>
+            </form>
 
-            <p>Your Hogname is: {chosenHogName.push("hogName")}</p>
-        </>
-      )
+              <p>Your Hogname is: {chosenHogName.push("hogName")}</p>
+
+            </AppShell.Main>
+        </AppShell>
+      </>
+    )
   
   }</MantineProvider>;
 }
